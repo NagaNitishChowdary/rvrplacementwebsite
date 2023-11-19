dca01d4be142f04b644dfe2d6f60d85adebb6975
Naga Nitish <95007559+NagaNitishChowdary@users.noreply.github.com>
2023-11-19 23:48:12 +0530

    Delete assets/css/js/placements-statistics.js

--- assets/css/js/placements-statistics.js
+++
@@ -1,7 +0,0 @@
-function myfunction(a) {
-    var tabcontent = document.getElementsByClassName("stat-table");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    document.getElementById(a).style.display="block";
-}
